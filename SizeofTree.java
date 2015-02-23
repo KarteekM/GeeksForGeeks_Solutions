12f6d968e8053b9b6c0e2ddfb814f567e41f8131
Karteek <mkarteek27@gmail.com>
2015-02-23 06:24:36 +0530

    Create SizeofTree.java
    Calculates size of the tree

---
+++ SizeofTree.java
@@ -0,0 +1,24 @@
+public class SizeofTree {
+	
+	public static int size(Node node){
+		if(node == null)
+			return 0;
+		
+		else
+			return 1 + size(node.left) + size(node.right);
+	}
+	
+	public static void main(String args[]){
+		Node root = new Node(1);
+	root.left = new Node(2);
+	root.right = new Node(3);
+	root.left.left = new Node(4);
+	root.left.right = new Node(5);
+	
+	System.out.println("Size of the tree is "+size(root));
+		
+		
+		
+	}
+
+}
