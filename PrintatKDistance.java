c582045342b39f10209e38b93ac891b85a038df8
Karteek <mkarteek27@gmail.com>
2015-03-06 06:10:17 +0530

    Create PrintatKDistance.java

---
+++ PrintatKDistance.java
@@ -0,0 +1,31 @@
+
+public class PrintatKDistance {
+
+	public static void printatK(Node node,int distance,int level){
+		
+		if(node == null)
+			return;
+		
+	    if(level == distance){
+	    	System.out.println(node.data);
+	    }
+	    
+	    else
+	    {
+	    	printatK(node.left,distance,level+1);
+	    	printatK(node.right,distance,level+1);
+	    }
+		
+	}
+	public static void main(String args[]){
+		
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left=new Node(4);
+		root.left.right= new Node(5);
+		root.right.left = new Node(8);
+		printatK(root,2,0);
+	}
+	
+}
