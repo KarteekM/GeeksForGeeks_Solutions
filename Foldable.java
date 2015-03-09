75fd3c64606491d612fff57e7b380772b6835e0b
Karteek <mkarteek27@gmail.com>
2015-03-09 06:39:42 +0530

    Create Foldable.java

---
+++ Foldable.java
@@ -0,0 +1,33 @@
+
+public class Foldable {
+	
+	public static boolean foldCheck(Node n1,Node n2){
+		
+		if(n1 == null && n2 == null){
+			return true;
+		}
+		
+		if( n1 == null || n2 == null){
+			return false;
+		}
+		
+			return foldCheck(n1.left,n2.right) && foldCheck(n1.right,n2.left);
+		
+	}
+	
+	public static boolean foldable(Node node){
+		
+		return foldCheck(node.left,node.right);
+	}
+
+	public static void main(String args[]){
+		
+		Node root =  new Node(1);
+		root.left =new Node(2);
+		root.right =new Node(3);
+		root.left.right = new Node(4);
+		root.right.left = new Node(5);
+		
+		System.out.println("Tree foldability is "+foldable(root));
+	}
+}
