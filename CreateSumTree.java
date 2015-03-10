2141759c56979a785ce4c9932a6e7ec6b2b3d799
Karteek <mkarteek27@gmail.com>
2015-03-10 06:32:35 +0530

    Update CreateSumTree.java

--- CreateSumTree.java
+++ CreateSumTree.java
@@ -1,4 +1,14 @@
-
+class Node{
+	
+	Node left;
+	int data;
+	Node right;
+	
+	Node(int data){
+		left =null;
+		this.data=data;
+		right=null;
+	}
 public class CreateSumTree {
 
 	public static int createSum(Node node){
