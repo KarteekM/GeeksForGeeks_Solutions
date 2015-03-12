614e5b875c0ade432c81326922f5325a9a516251
Karteek <mkarteek27@gmail.com>
2015-03-12 16:41:31 +0530

    Create IterativeAncestors.java

---
+++ IterativeAncestors.java
@@ -0,0 +1,53 @@
+
+public class IterativeAncestor {
+	
+	public static void iterativeAncestor(Node node,int data){
+		
+		if(node == null)
+			return ;
+		
+		Stack s4 = new Stack();
+		
+		
+		while(true){
+			
+			while(node!=null &&node.data !=data ){
+				
+				s4.push(node);
+				node = node.left;
+			}
+			
+			if( node!=null && node.data == data)
+				break;
+			
+			if(s4.peek().right == null){
+				
+				node = s4.pop();
+				
+				while(s4.top!= -1 && s4.peek().right == node){
+					node = s4.pop();
+				}
+			}
+			
+			node = (s4.top ==-1)? null:s4.peek().right;
+		}
+		
+		while(s4.top != -1){
+			System.out.println(s4.pop().data);
+			
+		}
+		
+	}
+	
+	public static void main(String args[]){
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.right = new Node(7);
+		iterativeAncestor(root,7);
+		
+	}
+
+}
