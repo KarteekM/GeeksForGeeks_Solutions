c091b6ae7bdf0cd5f5bcd80ccec7bd10f2d06573
Karteek <mkarteek27@gmail.com>
2015-03-14 18:31:06 +0530

    Create CheckFullBinaryTree.java

---
+++ CheckFullBinaryTree.java
@@ -0,0 +1,47 @@
+
+public class CheckFullBinaryTree {
+	
+	public static boolean checkFull(Node node){
+		
+		if(null ==  node)
+			return true;
+		
+		if(null == node.left && null == node.right)
+			return true;
+		
+		if(null!= node.left && null!= node.right){
+			return checkFull(node.left) &&	checkFull(node.right);
+		}
+		
+		return false;
+	}
+	
+	public static void main(String args[]){
+		
+		Node root = new Node(1);
+		root.left = new Node(20);
+	    root.right = new Node(30);
+	 
+	    root.left.right = new Node(40);
+	    root.left.left = new Node(50);
+	    root.right.left = new Node(60);
+	    root.right.right = new Node(70);
+	 
+	    root.left.left.left = new Node(80);
+	    root.left.left.right = new Node(90);
+	    root.left.right.left = new Node(80);
+	    root.left.right.right = new Node(90);
+	    root.right.left.left = new Node(80);
+	    root.right.left.right = new Node(90);
+	    root.right.right.left = new Node(80);
+	    root.right.right.right = new Node(90);
+	   // root.right.right.right.right = new Node(100);
+	    if(checkFull(root))
+	    	System.out.println("Tree is full");
+	    
+	    else
+	    	System.out.println("Tree is not full");
+	 
+	}
+
+}
