31adc0f5136bb13ac300afbf19fbb30b9384ee66
Karteek <mkarteek27@gmail.com>
2015-03-23 06:33:25 +0530

    Create InternalNodeisBST.java

---
+++ InternalNodeisBST.java
@@ -0,0 +1,40 @@
+
+public class InternalNodeisBST {
+
+	public static boolean internalNode(int []arr){
+		
+		if(arr.length<2)
+			return false;
+		
+		int min=0,max=0;
+		if(arr[arr.length-1] >arr[arr.length-2]){
+			max = arr[arr.length-1];
+			min  = arr[arr.length-2];
+		}
+		else
+		{ 
+			 max =arr[arr.length-2];
+			 min = arr[arr.length-1];
+			
+		}
+		
+		int i=0;
+		for(i=0;i<arr.length;i++){
+			
+			if(! (arr[i]>min || arr[i]<max)){
+				return false;
+			}
+			
+		}
+		
+			
+		return true;
+	}
+	
+	public static void main(String args[]){
+		
+		int pre[]={8,3,5,7,6};
+		System.out.println("Internal Node statement is" +internalNode(pre));
+		
+	}
+}
