cd9b8f86d2431d9ad360b6fd52431a68d962821f
Karteek <mkarteek27@gmail.com>
2015-04-03 07:03:20 +0530

    Update GreaterSBSTtoBinary.java

--- GreaterSBSTtoBinary.java
+++ GreaterSBSTtoBinary.java
@@ -1,4 +1,15 @@
 
+class Node{
+	
+	Node left;
+	int data;
+	Node right;
+	
+	Node(int data){
+		left =null;
+		this.data=data;
+		right=null;
+	}
 public class GreaterSBSTtoBinary {
 	
 	  public static int sumBST(Node node,int value){
