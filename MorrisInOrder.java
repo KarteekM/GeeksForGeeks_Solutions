3f0b86a22261c53bc686cdd76cb3e45a127dba52
Karteek <mkarteek27@gmail.com>
2015-04-04 20:53:48 +0530

    Update MorrisInOrder.java

--- MorrisInOrder.java
+++ MorrisInOrder.java
@@ -1,4 +1,14 @@
-
+class Node{
+	
+	Node left;
+	int data;
+	Node right;
+	
+	Node(int data){
+		left =null;
+		this.data=data;
+		right=null;
+	}
 public class MorrisInOrder {
 	
 	public static void M_Inorder(Node root){
