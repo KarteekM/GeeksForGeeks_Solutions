649093ebcba60c81d2bc96887f89bf54e4ca2298
Karteek <mkarteek27@gmail.com>
2015-04-04 19:22:17 +0530

    Create MorrisInOrder.java

---
+++ MorrisInOrder.java
@@ -0,0 +1,58 @@
+
+public class MorrisInOrder {
+	
+	public static void M_Inorder(Node root){
+		
+		Node pre=null;
+		if (root == null)
+			return;
+		
+		Node current =root;
+		
+		while(current != null){
+			
+			if(current.left == null){
+				System.out.println(current.data);
+				current =current.right;
+			}
+			
+			else
+			{
+				
+				pre=current.left;
+				
+				while(pre.right != null && pre.right != current){
+					pre=pre.right;
+				}
+				
+				if(pre.right == null){
+					pre.right=current;
+					current=current.left;
+				}
+				
+				else
+				{
+					pre.right = null;
+					System.out.println(current.data);
+					current= current.right;
+				}
+				
+			}
+			
+		}
+		
+	
+	}
+	
+	public static void main(String args[]){
+		
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		
+		M_Inorder(root);
+	}
+
+}
