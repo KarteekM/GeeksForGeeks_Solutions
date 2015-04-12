680d9cb4132d3f8c6294155318828afd44f06f4b
Karteek <mkarteek27@gmail.com>
2015-04-12 05:57:03 +0530

    Create OddandEvenLevelDifference.java

---
+++ OddandEvenLevelDifference.java
@@ -0,0 +1,27 @@
+
+public class OddandEvenLevelDifference {
+	
+	public static int diff(Node node){
+		
+		if(null  == node)
+			return 0;
+		
+		return node.data -diff(node.left) -diff(node.right);
+	};
+	
+	
+	public static void main(String args[]){
+		
+		Node node  = new Node(5);
+	    node.left = new Node(2);
+	    node.right = new Node(6);
+	    node.left.left  = new Node(1);
+	    node.left.right = new Node(4);
+	    node.left.right.left = new Node(3);
+	    node.right.right = new Node(8);
+	    node.right.right.right = new Node(9);
+	    node.right.right.left = new Node(7);
+			System.out.println("Difference is " +diff(node));
+	}
+
+}
