ff6f25d93955c04223d433238d848b65b0da2195
Karteek <mkarteek27@gmail.com>
2015-04-17 19:20:22 +0530

    Create KDistancefromLeaf.java

---
+++ KDistancefromLeaf.java
@@ -0,0 +1,46 @@
+
+public class KDistancefromLeaf {
+
+	
+	public static void traverseandprint(Node node,int path[],int index,int visited[],int distance){
+		
+		if(null == node)
+			return;
+		
+		path[index]=node.data;
+		visited[index]=1;
+		index++;
+		
+		if( node.left == null && node.right == null && index-distance-1>=0
+				&& visited[index-distance-1]==1){
+			System.out.println(path[index-distance-1]);
+			visited[index-distance-1]=0;
+		}
+		
+		
+		traverseandprint(node.left,path,index,visited,distance);
+		traverseandprint(node.right,path,index,visited,distance);
+	}
+	
+	public static void printnodes(Node node,int distance){
+		
+		int []path= new int[10];
+		int index=0;
+		int []visited = new int[10];
+		traverseandprint(node,path,index,visited,distance);
+	}
+	
+	public static void main(String args[]){
+		
+		Node node = new Node(1);
+		node.left = new Node(2);
+		node.right = new Node(3);
+		node.left.left = new Node(4);
+		node.left.right = new Node(5);
+		node.right.left = new Node(6);
+		node.right.right = new Node(7);
+		node.right.right.left = new Node(8);
+		
+		printnodes(node,2);
+	}
+}
