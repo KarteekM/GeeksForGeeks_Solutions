d59ac223ab125af4a03b2182b7f6cebcf2b4c31d
Karteek <mkarteek27@gmail.com>
2015-05-16 09:42:31 +0530

    Update SnakeandLadderPending.java

--- Algos/SnakeandLadderPending.java
+++ Algos/SnakeandLadderPending.java
@@ -1,3 +1,5 @@
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +16,21 @@ class Ladder{
 
 class Board{
 	
-	int position;
+	int position[];
 	boolean hasSnake;
 	boolean hasLadder;
-	Snake s;
-	Ladder l;
+	Snake snake[];
+	Ladder ladder[];
+	
+	Board(){
+		int i=0;
+		while(i<100){
+			 
+			 this.position[i]=i+1;
+			 this.snake[i]=null;
+			 this.ladder[i]=null;
+		 }
+	}
 	
 }
 
@@ -26,32 +38,59 @@ class Player{
 	
 	String name;
 	Board board;
+	int player_position;
 	
 	Player(String name){
 		this.name = name;
+		this.player_position = 1;
 	}
 	
 }
 
 public class SnakeandLadder {
-
+	
 	public static void main(String args[]){
 		
-		Player player1 = new Player("Player1");
-		Player player2 = new Player("Player2");
+		//Create Board
+		int i=0;		
+		Board playable_board = new Board();
 		
-		Board[] board = new Board[100];
-		int i=0;
-         System.out.println("Length is "+board.length);
-         
-         for(i=0;i<board.length;i++){
-        	 board[i].position = i+1;
-         }
-         
-         
+		//Define specific ladders
+		playable_board.ladder[5] = new Ladder();
+		playable_board.ladder[5].ladder.put(6, 99);
+		
+		playable_board.ladder[4] = new Ladder();
+		playable_board.ladder[4].ladder.put(5, 94);
+		
+		playable_board.ladder[3] = new Ladder();
+		playable_board.ladder[3].ladder.put(6, 96);
+	
+		
+		ArrayList<Player> playerList = new ArrayList<Player>();
+		
+		for(i=0;i<playerList.size();i++){
+			
+			Player player = new Player("Player " +i);			
+			 playerList.add(player);
+		}
 
+		
+		//Play the actual game
+		while(playerList.size()>0){
+			
+			for(i=0;i<playerList.size();i++){
+				
+				
+			}
+		}
+		 
+		
 		
 	}
 
-
+	
+	
+	
+	
+	
 }
