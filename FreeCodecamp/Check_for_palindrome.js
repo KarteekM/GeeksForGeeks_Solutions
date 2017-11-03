12da21df6334896ae5d25bc977df7ac6ea5851e8
Karteek <mkarteek27@gmail.com>
2017-11-03 17:47:27 +0530

    Create Check_for_palindrome.js

---
+++ FreeCodecamp/Check_for_palindrome.js
@@ -0,0 +1,16 @@
+function palindrome(str) {
+    str = str.toLowerCase().replace(/[\W_]/g, '');
+ var i=0;
+ var j = str.length-1;
+  for(;i<j;i++,j--){
+	  
+	  if(str[i] !== str[j])
+		  return false;
+  }
+ 
+  return true;
+}
+
+
+
+palindrome("eye");
