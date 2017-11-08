7aead29af8074e46d30323306d043d72a4b2f032
Karteek <mkarteek27@gmail.com>
2017-11-08 10:22:06 +0530

    Create DNA_Pairing.js

---
+++ FreeCodecamp/DNA_Pairing.js
@@ -0,0 +1,37 @@
+
+function pairElement(str) {
+
+var twodArray = [];
+var arrayToBePushed = [];
+
+for(var i=0;i<str.length;i++){
+	
+	if(str[i] == 'A'){
+     	  
+	  arrayToBePushed = [ "A","T"];
+	 
+	}
+	
+	else if(str[i] == 'T'){
+		arrayToBePushed = ["T","A"];
+		
+	}
+	
+	else if(str[i] == 'C'){
+	    arrayToBePushed = ["C","G"];
+		
+		
+	}
+	
+	else if(str[i] == 'G'){
+		arrayToBePushed = ["G","C"];
+		
+		
+	}
+  twodArray.push(arrayToBePushed);
+  
+}	
+
+	
+  return twodArray;
+}
