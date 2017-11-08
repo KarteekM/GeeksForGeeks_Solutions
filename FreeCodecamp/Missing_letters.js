fd96970dea950154e6586f65800dc9a4002a412f
Karteek <mkarteek27@gmail.com>
2017-11-08 21:21:23 +0530

    Create Missing_letters.js

---
+++ FreeCodecamp/Missing_letters.js
@@ -0,0 +1,17 @@
+function fearNotLetter(str) {
+  
+  var prev = str.charCodeAt(0);
+  
+  for(var i=1;i<str.length;i++){
+    
+      curr = str.charCodeAt(i);
+      if(curr - prev == 1){
+        prev = curr;
+      }
+    else{
+      return String.fromCharCode(prev+1);
+    }
+  }
+  
+  return undefined;
+}
