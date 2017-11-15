841dd865a9d05a323759390693454f3ecf531dd3
Karteek <mkarteek27@gmail.com>
2017-11-15 17:39:25 +0530

    Create Binary_Agents.js

---
+++ FreeCodecamp/Binary_Agents.js
@@ -0,0 +1,20 @@
+function binaryAgent(str) {
+  	var splitBinaryString = [];
+	splitBinaryString = str.split(' ');
+	convertedDecimalArray = [];
+	var finalsentence = '';
+	
+	 //convert binary to decimal
+	 var decimal = 0;
+	for(var i=0;i<splitBinaryString.length;i++){
+		for(j=0;j<splitBinaryString[i].length;j++){
+			
+			decimal = decimal + splitBinaryString[i][j]*Math.pow(2,splitBinaryString[i].length-1-j);
+		}
+		 finalsentence += (String.fromCharCode(decimal));
+		 decimal = 0;
+	}
+	
+  return finalsentence;
+  
+}
