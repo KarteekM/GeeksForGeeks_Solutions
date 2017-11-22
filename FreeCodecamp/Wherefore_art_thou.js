4de2076f1c895413d4d0cb114fde468c705885d6
Karteek <mkarteek27@gmail.com>
2017-11-22 15:43:31 +0530

    Create Wherefore_art_thou.js

---
+++ FreeCodecamp/Wherefore_art_thou.js
@@ -0,0 +1,22 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  
+ var sourceKeys = Object.keys(source);
+   
+arr = collection.filter(function(obj){
+
+for(var i=0;i<sourceKeys.length;i++){
+	if( !obj.hasOwnProperty(sourceKeys[i]) || obj[sourceKeys[i]] != source[sourceKeys[i]]){
+		return false;
+	}
+ }
+	return true;
+});   
+ 
+  
+  
+  // Only change code above this line
+  return arr;
+}
