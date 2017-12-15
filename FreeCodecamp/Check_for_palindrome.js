c3c3a186f27bd32d79af32f4d26d7fc79bf7b1f0
Karteek <mkarteek27@gmail.com>
2017-12-15 06:05:27 +0530

    Update Check_for_palindrome.js

--- FreeCodecamp/Check_for_palindrome.js
+++ FreeCodecamp/Check_for_palindrome.js
@@ -1,6 +1,5 @@
 function palindrome(str) {
-	
-    str = str.toLowerCase().replace(/[\W_]/g, '');
+	    str = str.toLowerCase().replace(/[\W_]/g, '');
  var i=0;
  var j = str.length-1;
   for(;i<j;i++,j--){
