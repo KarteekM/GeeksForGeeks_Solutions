87a3f33732b4432456dca269683be5cd80f03e26
Karteek <mkarteek27@gmail.com>
2017-12-27 20:45:34 +0530

    Update CheckFullBinaryTree.java

--- CheckFullBinaryTree.java
+++ CheckFullBinaryTree.java
@@ -34,6 +34,7 @@ public class CheckFullBinaryTree {
 	    root.right = new Node(30);
 	 
 	    root.left.right = new Node(40);
+		
 	    root.left.left = new Node(50);
 	    root.right.left = new Node(60);
 	    root.right.right = new Node(70);
